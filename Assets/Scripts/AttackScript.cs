c651ecafc7d8bd19a80e8a106b5ed8e23e4ec536
E.Delalande <delalande.ethan@gmail.com>
2023-04-07 12:00:44 +0200

    added health and barebones attack

--- Assets/Scripts/AttackScript.cs
+++ Assets/Scripts/AttackScript.cs
@@ -1,31 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 
 public class AttackScript : MonoBehaviour
-{
-    [SerializeField] public bool attacking = false;
-    [SerializeField] private Vector3 dir;
-    [SerializeField] private RigidbodyMovement movementScript;
+{   
+    private bool CD = false;
 
-    private void Start() {
-        DOTween.Init();
+    [SerializeField] private Transform check;
+    public AudioSource Source; //Player Audio Source
+    public AudioClip swordHit; //Sound clips
+    
+    void Update()
+    {
+        RaycastHit hit = new RaycastHit();
+        
+        if(Physics.Raycast(check.position, check.forward, out hit, 1) && !CD){
+            if(hit.collider.CompareTag("Enemy")){
+                Source.PlayOneShot(swordHit);
+                Debug.Log("Hit");
+                HealthScript HPscript = hit.collider.gameObject.GetComponent<HealthScript>();
+                HPscript.HP -= 25f;
+                StartCoroutine(attackCD(0.75f));
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator attackCD(float time)
     {   
-        dir = movementScript.moveVector;
-
-        if(Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            attacking = true;
-            transform.DOMove(new Vector3(dir.x, transform.position.y, dir.z), 0.5f);
-        }
-        if(Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            attacking = false;
-        }
+        CD = true;
+        yield return new WaitForSeconds(time);
+        CD = false;
     }
 }
